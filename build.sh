22a345f47ae0df4800814fc54e6e9eba2f0abefb
dominick <dom.serwe@gmail.com>
2023-03-06 17:47:30 -0500

    kernel entry - need i386 gcc cross compiler to link it together

--- build.sh
+++ build.sh
@@ -2,15 +2,22 @@
 
 # compile bootloader
 cd src/boot
+
 nasm -f bin turtle.asm -o boot.bin
 mv boot.bin ../../build
 
+# compile kernel entry
+nasm -f elf kernel_entry.asm -o kernel_entry.o
+mv kernel_entry.o ../../build
+
 # compile kernel
 cd ../kernel
+
 gcc -ffreestanding -c kernel.c -o kernel.o
 mv kernel.o ../../build
 
 cd ../../build
-ld -o kernel.bin kernel.o -Ttext 0x1000 --oformat binary
+ld -o kernel.bin -Ttext 0x1000 kernel_entry.o kernel.o --oformat binary
 
+# put it all together
 cat boot.bin kernel.bin > os.bin
